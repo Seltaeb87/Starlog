27bfc359b772bedfb35e17f7ef741b2c79779a52
Bastiaan Bitter <bastiaan@skybluemedia.nl>
2017-08-12 15:54:58 +0200

    simple sql database

---
+++ sql/starlog.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2017 at 02:38 PM
+-- Server version: 5.7.17
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `starlog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `ID` int(11) NOT NULL,
+  `object` tinytext NOT NULL,
+  `object_location` tinytext NOT NULL,
+  `object_type` tinytext NOT NULL,
+  `magnitude` float NOT NULL,
+  `equipment` text NOT NULL,
+  `observing_location` text NOT NULL,
+  `observing_date` datetime NOT NULL,
+  `notes` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`ID`, `object`, `object_location`, `object_type`, `magnitude`, `equipment`, `observing_location`, `observing_date`, `notes`) VALUES
+(1, 'Messier 1', 'Rechte klimming	05u34m31,97s\r\nDeclinatie	+22° 00\' 52,1\"', 'Nevel', 16.5, 'Skywatcher EQ5, APM 80/600 refractor', 'Hilversum', '2017-08-11 23:01:00', 'Faintly visible with the small telescope. Probably see it better when on a dark site.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `username` tinytext NOT NULL,
+  `first_name` tinytext NOT NULL,
+  `last_name` tinytext NOT NULL,
+  `password` tinytext NOT NULL,
+  `location` tinytext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
