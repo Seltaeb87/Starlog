e6b8ffcf82c50d6f225469ecc20932b7b82ac1ff
Bastiaan Bitter <bastiaan@skybluemedia.nl>
2017-08-15 22:23:21 +0200

    Added test data from dbconnect.php

--- index.php
+++ index.php
@@ -6,4 +6,34 @@
  * Time: 0:01
  */
 
-echo "Welcome to StarLog";
+require('dbconnect.php');
+
+echo "<b>Welcome to StarLog! this is the first setup, quick and dirty. Will refactor/update/refine after first things work!</b><br /><br />";
+
+
+/**
+ * Select all rows from table 'Logs'
+ */
+$sql = "SELECT * from logs";
+$result = $connection->query($sql);
+
+/**
+ * Get the results from observations list
+ */
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "Object: "          . utf8_encode($row['object']) . "<br />";
+        echo "Object location: " . utf8_encode($row['object_location']) . "<br />";
+        echo "Type object: "     . utf8_encode($row['object_type']) . "<br />";
+        echo "Magnitude: "       . utf8_encode($row['magnitude']) . "<br />";
+        echo "Equipment: "       . utf8_encode($row['equipment']) . "<br />";
+        echo "Observing site: "  . utf8_encode($row['observing_location']) . "<br />";
+        echo "Observing date: "  . utf8_encode($row['observing_date']) . "<br />";
+        echo "Notes: "           . utf8_encode($row['notes']) . "<br />";
+    }
+}
+
+/**
+ * CLose the database
+ */
+mysqli_close($connection);
